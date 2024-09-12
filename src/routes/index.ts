42670c3eadccb13c908b8601ee6f22ec3388eaea
DenysSidorov <000scorpions000@gmail.com>
2024-09-12 23:39:23 +0300

    added ability to create new todos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import { healthCheck } from '../handlers/healthcheck';
 import { data } from '../data/todos';
 import { TaskType } from '../types/task';
+import { TodoType } from '../types/todo';
+import { getCurrentDateTimeFormatted } from '../utils/dates';
 
 const router = express.Router();
 
@@ -22,26 +24,80 @@ router.get('/api/categories/:categoryId/tasks ', (req, res) => {
   res.send(category || {});
 });
 
-router.get('/api/categories/:categoryId/tasks/:taskId', (req, res) => {
+router.get('/api/categories/:categoryId/tasks/:taskId/todos', (req, res) => {
   const categoryId: string = req.params.categoryId;
   const taskId: string = req.params.taskId;
-  let task = {};
+  let todos: TodoType[] = [];
   const category = data.categories.find((el) => el.id === categoryId);
   if (category) {
     const categoryTasks = category?.tasks;
     if (categoryTasks) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      const currentTask: TaskType = categoryTasks.find((el: TaskType) => el.id === taskId);
-      if (currentTask) {
-        task = currentTask;
-        console.log(currentTask);
+      const task: TaskType | undefined = categoryTasks.find((el: TaskType) => el.id === taskId);
+      if (task) {
+        todos = task?.todos || [];
       }
+      console.log(task);
     }
     // const todos = category?.tasks?.find((el: TaskType) => el.id === taskId);
     // console.log(tasks);
   }
-  res.send(task);
+  res.send(todos);
+});
+
+router.post('/api/categories/:categoryId/tasks/:taskId/todos', async (req, res) => {
+  const categoryId: string = req.params.categoryId;
+  const taskId: string = req.params.taskId;
+  const { text } = req.body;
+
+  let result = false;
+
+  let indexCategory = -1;
+  let indexTask = -1;
+
+  const category = data.categories.find((el, index) => {
+    if (el.id === categoryId) {
+      indexCategory = index;
+      return true;
+    }
+    return false;
+  });
+  if (category) {
+    const categoryTasks = category?.tasks;
+    if (categoryTasks) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const task: TaskType | undefined = categoryTasks.find((el: TaskType, index: number) => {
+        if (el.id === taskId) {
+          indexTask = index;
+          return true;
+        }
+        return false;
+      });
+      if (task) {
+        const newTodo = {
+          id: Date.now().toString(),
+          name: text,
+          date: getCurrentDateTimeFormatted(),
+          completed: false,
+        };
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (data?.categories[indexCategory]?.tasks[indexTask]?.todos) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          data.categories[indexCategory].tasks[indexTask].todos.push(newTodo);
+        } else {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          data.categories[indexCategory].tasks[indexTask].todos = [newTodo];
+        }
+        result = true;
+      }
+    }
+  }
+  res.send(result);
 });
 
 router.patch('/api/categories/:id', (req, res) => {
